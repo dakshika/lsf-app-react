0003b4f4675118ea62be004f2c9c5da1aecd072b
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2018-11-20 15:59:04 +0530

    initial commit

---
+++ src/app/ProtectedApp.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Protected extends Component {
+    render() {
+        return (
+            /* app level routers needs to handle here*/
+            <div>Protected page</div>
+        );
+
+    }
+}
